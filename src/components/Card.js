72b9f61102a773f6a24fe39aa40bb08ca35ac2ed
calvinarieri <ariericalvin@gmail.com>
2024-09-23 15:43:53 +0300

    UI improvement:services section

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ export default function Card(){
     return(
         <div className="border-2 border-gray-300 w-full sm:w-96 p-4 flex flex-col justify-center items-center tracking-widest font-DMSans rounded">
             <h1 className="border-b-2 pb-4 font-bold text-2xl w-full text-center">Service title</h1>
-            <div className="flex gap-2 items-end my-8">
+            <div className="flex gap-2 items-end my-4 md:my-8">
                 <h2 className="text-4xl text-gray-600 tracking-wide">40KES</h2>
                 <span className="text-2xl font-bold text-gray-500">/kg</span>
             </div>
--- src/components/Services.js
+++ src/components/Services.js
@@ -4,9 +4,9 @@ export default function Services(){
 
     const services = ["1", "2", "3"]
     return(
-        <div id="services" className="flex flex-col justify-center items-center md:my-28 font-DMSans">
+        <div id="services" className="flex flex-col md:justify-center items-center md:my-28 font-DMSans">
             <h1 className="font-bold md:text-6xl text-blue-400 mt-4 border-b-2 pb-4 text-4xl border-blue-500 px-9">Our services</h1>
-            <div className="flex flex-col  md:flex-row gap-8 mt-10 md:mt-20">
+            <div className="flex flex-col px-2 md:justify-center md:flex-row gap-4 md:gap-8 mt-10 md:mt-20 w-full">
                 {services.map((service , index)=><Card />)}
             </div>
         </div>
