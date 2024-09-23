6d805ffd4bd6f93a917502d70a4320e132e35871
calvinarieri <ariericalvin@gmail.com>
2024-09-23 15:47:34 +0300

    UI improvement:services section

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 export default function Card(){
     return(
         <div className="border-2 border-gray-300 w-full sm:w-96 p-4 flex flex-col justify-center items-center tracking-widest font-DMSans rounded">
-            <h1 className="border-b-2 pb-4 font-bold text-2xl w-full text-center">Service title</h1>
+            <h1 className="border-b-2 md:pb-4 pb-2 font-bold md:text-2xl w-full text-center">Service title</h1>
             <div className="flex gap-2 items-end my-4 md:my-8">
-                <h2 className="text-4xl text-gray-600 tracking-wide">40KES</h2>
-                <span className="text-2xl font-bold text-gray-500">/kg</span>
+                <h2 className="md:text-4xl text-2xl text-gray-600 tracking-wide">40KES</h2>
+                <span className="text-xl font-bold text-gray-500">/kg</span>
             </div>
-            <button className="bg-blue-500 hover:bg-blue-400 w-full text-white px-4 py-2 font-bold text-lg tracking-widest mb-4 rounded">Order now</button>
+            <button className="bg-blue-500 hover:bg-blue-400 w-full text-white px-4 py-2 font-bold text-lg tracking-widest md:mb-4 rounded">Order now</button>
         </div>
     )
 }
